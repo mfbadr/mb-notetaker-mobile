b6e3ff0513245d98b84ba547018c578b4c673bad
Mikey Badr <mfbadr@gmail.com>
2014-12-10 11:58:52 -0600

    choose photo from phone

--- www/js/controllers/note-detail-controller.js
+++ www/js/controllers/note-detail-controller.js
@@ -10,5 +10,30 @@
           console.log('something went wrong', response);
       });
 
+      function success(b64){
+        console.log(b64);
+      }
+      function error(msg){
+        console.log(msg);
+      }
+
+      $scope.snap = function(){
+        var options = {
+          quality: 100,
+          destinationType: Camera.DestinationType.DATA_URL
+        };
+        navigator.camera.getPicture(success, error, options);
+      };
+
+      $scope.choose = function(){
+        var options = {
+          quality: 100,
+          sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+          destinationType: Camera.DestinationType.DATA_URL
+        };
+        navigator.camera.getPicture(success, error, options);
+      };
+
+
     }]);
 })();
